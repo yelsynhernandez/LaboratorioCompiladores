f97fc85850c8997b6dc54295958cbdb6911432c6
Yelsyn Hernández <yelsynhernandez@hotmail.com>
2024-03-11 19:01:50 -0600

    Commit inicial

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace LaboratorioCompiladores
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tlpPrincipal = new System.Windows.Forms.TableLayoutPanel();
+            this.lblContenidoArchivo = new System.Windows.Forms.Label();
+            this.lblVariables = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblTerminales = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.tlpPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(691, 491);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(84, 27);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(51, 167);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(336, 270);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tlpPrincipal
+            // 
+            this.tlpPrincipal.ColumnCount = 7;
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.882352F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.17646F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.882352F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.64706F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.882352F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.64706F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.882352F));
+            this.tlpPrincipal.Controls.Add(this.textBox2, 1, 3);
+            this.tlpPrincipal.Controls.Add(this.lblContenidoArchivo, 1, 2);
+            this.tlpPrincipal.Controls.Add(this.lblVariables, 3, 2);
+            this.tlpPrincipal.Controls.Add(this.textBox1, 3, 3);
+            this.tlpPrincipal.Controls.Add(this.textBox3, 5, 3);
+            this.tlpPrincipal.Controls.Add(this.lblTerminales, 5, 2);
+            this.tlpPrincipal.Controls.Add(this.btnSalir, 5, 4);
+            this.tlpPrincipal.Controls.Add(this.txtArchivo, 1, 1);
+            this.tlpPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tlpPrincipal.Name = "tlpPrincipal";
+            this.tlpPrincipal.RowCount = 6;
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tlpPrincipal.Size = new System.Drawing.Size(832, 553);
+            this.tlpPrincipal.TabIndex = 4;
+            // 
+            // lblContenidoArchivo
+            // 
+            this.lblContenidoArchivo.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblContenidoArchivo.AutoSize = true;
+            this.lblContenidoArchivo.Location = new System.Drawing.Point(135, 145);
+            this.lblContenidoArchivo.Name = "lblContenidoArchivo";
+            this.lblContenidoArchivo.Size = new System.Drawing.Size(168, 19);
+            this.lblContenidoArchivo.TabIndex = 4;
+            this.lblContenidoArchivo.Text = "Contenido del Archivo";
+            // 
+            // lblVariables
+            // 
+            this.lblVariables.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblVariables.AutoSize = true;
+            this.lblVariables.Location = new System.Drawing.Point(473, 145);
+            this.lblVariables.Name = "lblVariables";
+            this.lblVariables.Size = new System.Drawing.Size(76, 19);
+            this.lblVariables.TabIndex = 5;
+            this.lblVariables.Text = "Variables";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(441, 167);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 270);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(635, 167);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 270);
+            this.textBox3.TabIndex = 7;
+            // 
+            // lblTerminales
+            // 
+            this.lblTerminales.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblTerminales.AutoSize = true;
+            this.lblTerminales.Location = new System.Drawing.Point(662, 145);
+            this.lblTerminales.Name = "lblTerminales";
+            this.lblTerminales.Size = new System.Drawing.Size(86, 19);
+            this.lblTerminales.TabIndex = 8;
+            this.lblTerminales.Text = "Terminales";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpPrincipal.SetColumnSpan(this.txtArchivo, 5);
+            this.txtArchivo.Font = new System.Drawing.Font("Arial", 13F);
+            this.txtArchivo.Location = new System.Drawing.Point(51, 52);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(724, 32);
+            this.txtArchivo.TabIndex = 9;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 553);
+            this.Controls.Add(this.tlpPrincipal);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laboratorio 1 - Compiladores";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.tlpPrincipal.ResumeLayout(false);
+            this.tlpPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tlpPrincipal;
+        private System.Windows.Forms.Label lblContenidoArchivo;
+        private System.Windows.Forms.Label lblVariables;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblTerminales;
+        private System.Windows.Forms.TextBox txtArchivo;
+    }
+}
+
---
+++ FrmPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LaboratorioCompiladores
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpiarControles()
+        {
+            this.txtArchivo.ForeColor = System.Drawing.Color.Gray;
+            this.txtArchivo.Text = "Arrastre el archivo aquí para cargarlo";
+            this.txtArchivo.Font = System.Data
+        }
+
+    }
+}
